50e1387ca83c15cfced7c88c01ca332693a330c3
John Eisenmann <esperantanaso@gmail.com>
2015-04-05 14:38:21 -0400

    INSERTION SORT THE 99 RESTAURANT YOU'LL ALWAYS COME BACK FOR MORE

--- Sorting/src/Sorting/InsertionSort.java
+++ Sorting/src/Sorting/InsertionSort.java
@@ -2,14 +2,30 @@ package Sorting;
 
 public class InsertionSort {
 	/**
-	 * insertionSort			sorts an array of integers using insertion sort
+	 * insertionSort sorts an array of integers using insertion sort
 	 * 
-	 * @param unsortedArray		an unsorted array of integers
-	 * @return					the input array, sorted least to greatest
+	 * @param unsortedArray
+	 *          an unsorted array of integers
+	 * @return the input array, sorted least to greatest
 	 */
 	public static int[] insertionSort(int[] unsortedArray) {
-		//TODO: S2 implements insertion sort
-		
-		return null;
+		int[] output = new int[unsortedArray.length];
+		int index = 0;
+		while (index < unsortedArray.length) {
+			int tempNumber = unsortedArray[index];
+			int tempIndex = index;
+			while (tempIndex > 0) {
+				int tempNumber2 = output[tempIndex - 1];
+				if (tempNumber2 < tempNumber) {
+					break;
+				} else {
+					output[tempIndex] = tempNumber;
+				}
+				tempIndex -= 1;
+			}
+			output[tempIndex] = tempNumber;
+			index += 1;
+		}
+		return output;
 	}
 }
